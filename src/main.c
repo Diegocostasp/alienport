c9bd32d2c9184d5bafc42ef81b427f26cdcfbdc5
Diegocostasp <diegocosta199123@gmail.com>
2026-05-04 20:49:23 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 /*
  * main.c -- Alien Shooter ARM64 Linux loader
- *
- * Loads libalien_shooter.so and simulates ANativeActivity environment.
- * Game manages its own EGL/GL context. We only provide SDL window + lifecycle.
+ * EGL created via SDL, game's EGL calls are hooked to use our context.
  */
-
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_syswm.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,106 +21,89 @@
 #define SO_NAME "lib/arm64-v8a/libalien_shooter.so"
 #define MEMORY_MB 256
 
+/* Globals exported for EGL hooks in imports_extra.c */
+SDL_Window *g_sdl_window = NULL;
+SDL_GLContext g_sdl_glctx = NULL;
+EGLDisplay g_egl_display = EGL_NO_DISPLAY;
+EGLSurface g_egl_surface = EGL_NO_SURFACE;
+EGLContext g_egl_context = EGL_NO_CONTEXT;
+int g_screen_w = 640;
+int g_screen_h = 480;
+
 /* ANativeActivity types */
 typedef struct ANativeActivity ANativeActivity;
-
 typedef struct ANativeActivityCallbacks {
-    void (*onStart)(ANativeActivity *activity);
-    void (*onResume)(ANativeActivity *activity);
-    void *(*onSaveInstanceState)(ANativeActivity *activity, size_t *outSize);
-    void (*onPause)(ANativeActivity *activity);
-    void (*onStop)(ANativeActivity *activity);
-    void (*onDestroy)(ANativeActivity *activity);
-    void (*onWindowFocusChanged)(ANativeActivity *activity, int hasFocus);
-    void (*onNativeWindowCreated)(ANativeActivity *activity, void *window);
-    void (*onNativeWindowResized)(ANativeActivity *activity, void *window);
-    void (*onNativeWindowRedrawNeeded)(ANativeActivity *activity, void *window);
-    void (*onNativeWindowDestroyed)(ANativeActivity *activity, void *window);
-    void (*onInputQueueCreated)(ANativeActivity *activity, void *queue);
-    void (*onInputQueueDestroyed)(ANativeActivity *activity, void *queue);
-    void (*onContentRectChanged)(ANativeActivity *activity, const void *rect);
-    void (*onConfigurationChanged)(ANativeActivity *activity);
-    void (*onLowMemory)(ANativeActivity *activity);
+    void (*onStart)(ANativeActivity *a);
+    void (*onResume)(ANativeActivity *a);
+    void *(*onSaveInstanceState)(ANativeActivity *a, size_t *sz);
+    void (*onPause)(ANativeActivity *a);
+    void (*onStop)(ANativeActivity *a);
+    void (*onDestroy)(ANativeActivity *a);
+    void (*onWindowFocusChanged)(ANativeActivity *a, int f);
+    void (*onNativeWindowCreated)(ANativeActivity *a, void *w);
+    void (*onNativeWindowResized)(ANativeActivity *a, void *w);
+    void (*onNativeWindowRedrawNeeded)(ANativeActivity *a, void *w);
+    void (*onNativeWindowDestroyed)(ANativeActivity *a, void *w);
+    void (*onInputQueueCreated)(ANativeActivity *a, void *q);
+    void (*onInputQueueDestroyed)(ANativeActivity *a, void *q);
+    void (*onContentRectChanged)(ANativeActivity *a, const void *r);
+    void (*onConfigurationChanged)(ANativeActivity *a);
+    void (*onLowMemory)(ANativeActivity *a);
 } ANativeActivityCallbacks;
 
 struct ANativeActivity {
     ANativeActivityCallbacks *callbacks;
-    void *vm;
-    void *env;
-    void *clazz;
-    const char *internalDataPath;
-    const char *externalDataPath;
-    int32_t sdkVersion;
-    void *instance;
-    void *assetManager;
-    const char *obbPath;
+    void *vm; void *env; void *clazz;
+    const char *internalDataPath; const char *externalDataPath;
+    int32_t sdkVersion; void *instance; void *assetManager; const char *obbPath;
 };
 
 /* Fake JNI */
-static jint fake_GetVersion(JNIEnv *env) { return JNI_VERSION_1_6; }
-static jclass fake_FindClass(JNIEnv *env, const char *name) {
-    debugPrintf("JNI FindClass: %s\n", name);
-    return (jclass)0x41414141;
-}
-static jmethodID fake_GetMethodID(JNIEnv *env, jclass cls, const char *name, const char *sig) {
-    debugPrintf("JNI GetMethodID: %s %s\n", name, sig);
-    return (jmethodID)0x42424242;
-}
-static jmethodID fake_GetStaticMethodID(JNIEnv *env, jclass cls, const char *name, const char *sig) {
-    return (jmethodID)0x43434343;
-}
-static jfieldID fake_GetFieldID(JNIEnv *env, jclass cls, const char *name, const char *sig) {
-    return (jfieldID)0x44444444;
-}
-static jstring fake_NewStringUTF(JNIEnv *env, const char *str) {
-    return (jstring)(uintptr_t)str;
-}
-static const char *fake_GetStringUTFChars(JNIEnv *env, jstring str, jboolean *isCopy) {
-    if (isCopy) *isCopy = JNI_FALSE;
-    return (const char *)(uintptr_t)str;
-}
-static void fake_ReleaseStringUTFChars(JNIEnv *env, jstring str, const char *chars) { }
-static jobject fake_NewGlobalRef(JNIEnv *env, jobject obj) { return obj; }
-static void fake_DeleteGlobalRef(JNIEnv *env, jobject obj) { }
-static void fake_DeleteLocalRef(JNIEnv *env, jobject obj) { }
-static jboolean fake_ExceptionCheck(JNIEnv *env) { return JNI_FALSE; }
-static void fake_ExceptionClear(JNIEnv *env) { }
-static jint fake_GetJavaVM(JNIEnv *env, JavaVM **vm) { return JNI_OK; }
-static jint fake_AttachCurrentThread(JavaVM *vm, JNIEnv **env, void *args) { return JNI_OK; }
-static jint fake_DetachCurrentThread(JavaVM *vm) { return JNI_OK; }
-static jint fake_GetEnv(JavaVM *vm, void **env, jint version) { return JNI_OK; }
+static jint fake_GetVersion(JNIEnv *e) { return JNI_VERSION_1_6; }
+static jclass fake_FindClass(JNIEnv *e, const char *n) { return (jclass)0x41414141; }
+static jmethodID fake_GetMethodID(JNIEnv *e, jclass c, const char *n, const char *s) { return (jmethodID)0x42424242; }
+static jmethodID fake_GetStaticMethodID(JNIEnv *e, jclass c, const char *n, const char *s) { return (jmethodID)0x43434343; }
+static jfieldID fake_GetFieldID(JNIEnv *e, jclass c, const char *n, const char *s) { return (jfieldID)0x44444444; }
+static jstring fake_NewStringUTF(JNIEnv *e, const char *s) { return (jstring)(uintptr_t)s; }
+static const char *fake_GetStringUTFChars(JNIEnv *e, jstring s, jboolean *c) { if(c) *c=0; return (const char*)(uintptr_t)s; }
+static void fake_ReleaseStringUTFChars(JNIEnv *e, jstring s, const char *c) {}
+static jobject fake_NewGlobalRef(JNIEnv *e, jobject o) { return o; }
+static void fake_DeleteGlobalRef(JNIEnv *e, jobject o) {}
+static void fake_DeleteLocalRef(JNIEnv *e, jobject o) {}
+static jboolean fake_ExceptionCheck(JNIEnv *e) { return 0; }
+static void fake_ExceptionClear(JNIEnv *e) {}
+static jint fake_GetJavaVM(JNIEnv *e, JavaVM **v) { return 0; }
+static jint fake_AttachCurrentThread(JavaVM *v, JNIEnv **e, void *a) { return 0; }
+static jint fake_DetachCurrentThread(JavaVM *v) { return 0; }
+static jint fake_GetEnv(JavaVM *v, void **e, jint ver) { return 0; }
 
-static struct JNINativeInterface_ jni_functions = {0};
-static struct JNIInvokeInterface_ jvm_functions = {0};
+static struct JNINativeInterface_ jni_funcs = {0};
+static struct JNIInvokeInterface_ jvm_funcs = {0};
 static JNIEnv jni_env;
 static JavaVM jvm;
 
 static void jni_init(void) {
-    memset(&jni_functions, 0, sizeof(jni_functions));
-    jni_functions.GetVersion = fake_GetVersion;
-    jni_functions.FindClass = fake_FindClass;
-    jni_functions.GetMethodID = fake_GetMethodID;
-    jni_functions.GetStaticMethodID = fake_GetStaticMethodID;
-    jni_functions.GetFieldID = fake_GetFieldID;
-    jni_functions.NewStringUTF = fake_NewStringUTF;
-    jni_functions.GetStringUTFChars = fake_GetStringUTFChars;
-    jni_functions.ReleaseStringUTFChars = fake_ReleaseStringUTFChars;
-    jni_functions.NewGlobalRef = fake_NewGlobalRef;
-    jni_functions.DeleteGlobalRef = fake_DeleteGlobalRef;
-    jni_functions.DeleteLocalRef = fake_DeleteLocalRef;
-    jni_functions.ExceptionCheck = fake_ExceptionCheck;
-    jni_functions.ExceptionClear = fake_ExceptionClear;
-    jni_functions.GetJavaVM = fake_GetJavaVM;
-
-    jvm_functions.AttachCurrentThread = fake_AttachCurrentThread;
-    jvm_functions.DetachCurrentThread = fake_DetachCurrentThread;
-    jvm_functions.GetEnv = fake_GetEnv;
-
-    jni_env = &jni_functions;
-    jvm = &jvm_functions;
+    memset(&jni_funcs, 0, sizeof(jni_funcs));
+    jni_funcs.GetVersion = fake_GetVersion;
+    jni_funcs.FindClass = fake_FindClass;
+    jni_funcs.GetMethodID = fake_GetMethodID;
+    jni_funcs.GetStaticMethodID = fake_GetStaticMethodID;
+    jni_funcs.GetFieldID = fake_GetFieldID;
+    jni_funcs.NewStringUTF = fake_NewStringUTF;
+    jni_funcs.GetStringUTFChars = fake_GetStringUTFChars;
+    jni_funcs.ReleaseStringUTFChars = fake_ReleaseStringUTFChars;
+    jni_funcs.NewGlobalRef = fake_NewGlobalRef;
+    jni_funcs.DeleteGlobalRef = fake_DeleteGlobalRef;
+    jni_funcs.DeleteLocalRef = fake_DeleteLocalRef;
+    jni_funcs.ExceptionCheck = fake_ExceptionCheck;
+    jni_funcs.ExceptionClear = fake_ExceptionClear;
+    jni_funcs.GetJavaVM = fake_GetJavaVM;
+    jvm_funcs.AttachCurrentThread = fake_AttachCurrentThread;
+    jvm_funcs.DetachCurrentThread = fake_DetachCurrentThread;
+    jvm_funcs.GetEnv = fake_GetEnv;
+    jni_env = &jni_funcs;
+    jvm = &jvm_funcs;
 }
-
-static SDL_Window *sdl_window = NULL;
 
 typedef void (*ANativeActivity_createFunc)(ANativeActivity *, void *, size_t);
 
@@ -139,15 +120,10 @@ int main(int argc, char *argv[]) {
     size_t heap_size = MEMORY_MB * 1024 * 1024;
     void *heap = mmap(NULL, heap_size, PROT_READ | PROT_WRITE,
                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (heap == MAP_FAILED) {
-        debugPrintf("Failed to allocate %d MB\n", MEMORY_MB);
-        return 1;
-    }
+    if (heap == MAP_FAILED) { debugPrintf("mmap failed\n"); return 1; }
 
-    debugPrintf("Loading %s...\n", SO_NAME);
     if (so_load(SO_NAME, heap, heap_size) < 0) {
-        debugPrintf("Failed to load .so\n");
-        return 1;
+        debugPrintf("so_load failed\n"); return 1;
     }
     debugPrintf("so_load() passed.\n");
 
@@ -159,8 +135,6 @@ int main(int argc, char *argv[]) {
     debugPrintf("so_resolve() passed.\n");
 
     so_finalize();
-    debugPrintf("so_finalize() passed.\n");
-
     so_flush_caches();
     so_execute_init_array();
     debugPrintf("Init arrays executed.\n");
@@ -168,32 +142,53 @@ int main(int argc, char *argv[]) {
     jni_init();
     debugPrintf("jni_init() passed.\n");
 
-    /* SDL only - game creates its own EGL context */
+    /* Create SDL window + GL context */
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
         debugPrintf("SDL_Init failed: %s\n", SDL_GetError());
         return 1;
     }
 
-    sdl_window = SDL_CreateWindow("Alien Shooter",
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+
+    g_sdl_window = SDL_CreateWindow("Alien Shooter",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (!sdl_window) {
+    if (!g_sdl_window) {
         debugPrintf("SDL_CreateWindow failed: %s\n", SDL_GetError());
         return 1;
     }
-    debugPrintf("SDL initialized, driver: %s\n", SDL_GetCurrentVideoDriver());
+    SDL_GetWindowSize(g_sdl_window, &g_screen_w, &g_screen_h);
+    debugPrintf("Window: %dx%d, driver: %s\n", g_screen_w, g_screen_h, SDL_GetCurrentVideoDriver());
+
+    g_sdl_glctx = SDL_GL_CreateContext(g_sdl_window);
+    if (!g_sdl_glctx) {
+        debugPrintf("SDL_GL_CreateContext failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /* Grab the EGL handles SDL created internally */
+    g_egl_display = eglGetCurrentDisplay();
+    g_egl_surface = eglGetCurrentSurface(EGL_DRAW);
+    g_egl_context = eglGetCurrentContext();
+    debugPrintf("EGL: display=%p surface=%p context=%p\n", g_egl_display, g_egl_surface, g_egl_context);
+    debugPrintf("GL: %s\n", glGetString(GL_VERSION));
+
+    /* Unbind so the game thread can use it */
+    eglMakeCurrent(g_egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
 
     ANativeActivity_createFunc onCreate =
         (ANativeActivity_createFunc)so_find_addr("ANativeActivity_onCreate");
-    if (!onCreate) {
-        debugPrintf("ANativeActivity_onCreate not found!\n");
-        return 1;
-    }
+    if (!onCreate) { debugPrintf("onCreate not found!\n"); return 1; }
     debugPrintf("ANativeActivity_onCreate at %p\n", onCreate);
 
     ANativeActivityCallbacks callbacks;
     memset(&callbacks, 0, sizeof(callbacks));
-
     ANativeActivity activity;
     memset(&activity, 0, sizeof(activity));
     activity.callbacks = &callbacks;
@@ -210,27 +205,17 @@ int main(int argc, char *argv[]) {
     onCreate(&activity, NULL, 0);
     debugPrintf("ANativeActivity_onCreate returned.\n");
 
-    /* Wait for game thread to start its looper */
     usleep(500000);
 
-    debugPrintf("Sending lifecycle events...\n");
+    debugPrintf("Sending lifecycle...\n");
     if (callbacks.onNativeWindowCreated) {
-        callbacks.onNativeWindowCreated(&activity, sdl_window);
+        callbacks.onNativeWindowCreated(&activity, g_sdl_window);
         debugPrintf("  onNativeWindowCreated\n");
     }
-    usleep(100000);
-    if (callbacks.onStart) {
-        callbacks.onStart(&activity);
-        debugPrintf("  onStart\n");
-    }
-    if (callbacks.onResume) {
-        callbacks.onResume(&activity);
-        debugPrintf("  onResume\n");
-    }
-    if (callbacks.onWindowFocusChanged) {
-        callbacks.onWindowFocusChanged(&activity, 1);
-        debugPrintf("  onWindowFocusChanged\n");
-    }
+    usleep(200000);
+    if (callbacks.onStart) { callbacks.onStart(&activity); debugPrintf("  onStart\n"); }
+    if (callbacks.onResume) { callbacks.onResume(&activity); debugPrintf("  onResume\n"); }
+    if (callbacks.onWindowFocusChanged) { callbacks.onWindowFocusChanged(&activity, 1); debugPrintf("  onWindowFocusChanged\n"); }
 
     debugPrintf("Entering main loop...\n");
     SDL_Event event;
@@ -242,9 +227,10 @@ int main(int argc, char *argv[]) {
         SDL_Delay(16);
     }
 
-    if (callbacks.onPause)  callbacks.onPause(&activity);
-    if (callbacks.onStop)   callbacks.onStop(&activity);
-    SDL_DestroyWindow(sdl_window);
+    if (callbacks.onPause) callbacks.onPause(&activity);
+    if (callbacks.onStop) callbacks.onStop(&activity);
+    SDL_GL_DeleteContext(g_sdl_glctx);
+    SDL_DestroyWindow(g_sdl_window);
     SDL_Quit();
     return 0;
 }
